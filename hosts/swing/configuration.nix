d38f161aafa46df802bffc26671a321da758bf04
Simon Pitt <si_pitt@yahoo.com>
2024-08-25 18:38:05 +0100

    Start swing

--- hosts/swing/configuration.nix
+++ hosts/swing/configuration.nix
@@ -112,6 +112,8 @@
    slurp # screenshot functionality
    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
    mako # notification system developed by swaywm maintainer
+   chromium
+   pass
   ];
 
   # Install firefox.
