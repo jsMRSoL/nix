4b274155359f763e75c05efc29ab05a6061fe968
Simon Pitt <si_pitt@yahoo.com>
2024-08-27 11:22:17 +0100

    modularise

--- homemanager/home.nix
+++ homemanager/home.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ...}: {
+
   imports = [
-    ../modules/foot/foot.nix
+    ../modules/foot
     ../modules/bash
+    ../modules/git
+    ../modules/dwm-status
+    ../modules/games
   ];
+
   home = {
     username = "simon";
     homeDirectory = "/home/simon";
@@ -13,6 +18,7 @@
       bash-preexec
       exiftool
       htop
+      imagemagick
       lazygit
       mediainfo
       mpv
@@ -23,66 +29,9 @@
       xdg-utils
       yazi
       zoxide
-      superTux
-      imagemagick
     ];
   };
 
   programs.home-manager.enable = true;
 
-  programs.git = {
-    enable = true;
-    userEmail = "si_pitt@yahoo.com";
-    userName = "Simon Pitt";
-  };
-
-  services.dwm-status = {
-    enable = true;
-    package = pkgs.dwm-status;
-    # order = [ "audio" "backlight" "battery" "network" "cpu_load" "time" ];
-    extraConfig = {
-      order =  [ "cpu_load" "audio" "battery" "backlight" "network" "time" ];
-      separator =  " | ";
-      audio =  {
-        control =  "Master";
-        mute =  "MUTE";
-        template =  "S {VOL}%";
-        icons =  [];
-      };
-      backlight =  {
-        device =  "intel_backlight";
-        fallback =  "acpi_video*";
-        template =  "L {BL}%";
-        icons =  [];
-      };
-      battery =  {
-        charging =  "▲";
-        discharging =  "▼";
-        enable_notifier =  true;
-        no_battery =  "NO BATT";
-        notifier_critical =  10;
-        notifier_levels =  [ 2 5 10 15 20 ];
-        separator =  " · ";
-        icons =  [];
-      };
-      cpu_load =  {
-        template =  " {CL1} {CL5} {CL15}";
-        update_interval =  20;
-      };
-      network =  {
-        no_value =  "NA";
-        template =  "{ESSID}";
-      };
-      time =  {
-        format =  "%Y-%m-%d %H:%M:%S";
-        update_seconds =  true;
-      };
-    };
-  };
-
-  xdg.configFile."systemd/user/dwm-status.service.d/override.conf".text = ''
-    [Service]
-    Restart=always
-    RestartSec=10
-  '';
 }
---
+++ modules/git/default.nix
@@ -0,0 +1,7 @@
+{
+  programs.git = {
+    enable = true;
+    userEmail = "si_pitt@yahoo.com";
+    userName = "Simon Pitt";
+  };
+}
