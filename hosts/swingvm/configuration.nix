17da6b7a957507da93a268c65e0c43a495dc0a3e
Simon <si_pitty@yahoo.com>
2024-08-28 23:59:00 +0100

    get bootloader right

--- hosts/swingvm/configuration.nix
+++ hosts/swingvm/configuration.nix
@@ -12,9 +12,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "swing"; # Define your hostname.
 
