c1e45a62ed2bdc7136550b07168c7bb62c8a7934
Simon Pitt <si_pitt@yahoo.com>
2024-08-30 23:20:37 +0100

    realize you added the changes to the wrong configuration, go back and fix it...

--- flake.nix
+++ flake.nix
@@ -22,24 +22,26 @@
         };
         swing = nixpkgs.lib.nixosSystem {
           inherit system;
-          modules = [ ./hosts/swing/configuration.nix ];
+          modules = [ 
+            ./hosts/swing/configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPkgs = true;
+              home-manager.users.simon = import ./homemanager/home.nix;
+            }
+          ];
         };
         swingvm = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
             ./hosts/swingvm/configuration.nix
-            # home-manager.nixosModules.home-manager {
-            #   home-manager.useGlobalPkgs = true;
-            #   home-manager.useUserPkgs = true;
-            #   home-manager.users.simon = import ./homemanager/home.nix;
-            # }
           ];
         };
       };
 
-      homeConfigurations.simon = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ ./homemanager/home.nix ];
-      };
+      # homeConfigurations.simon = home-manager.lib.homeManagerConfiguration {
+      #   pkgs = nixpkgs.legacyPackages.${system};
+      #   modules = [ ./homemanager/home.nix ];
+      # };
     };
 }
