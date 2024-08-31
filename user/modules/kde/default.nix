360c296c3b01483ceb75e897cdd1eca2613eb955
Simon Pitt <si_pitt@yahoo.com>
2024-08-31 21:41:43 +0100

    add kwinrc & kxbrc

--- user/modules/kde/default.nix
+++ user/modules/kde/default.nix
@@ -4,4 +4,16 @@
     source = ./kglobalshortcutsrc;
     target = "kglobalshortcutsrc";
   };
+
+  xdg.configFile."kwinrc" = {
+    enable = true;
+    source = ./kwinrc
+    target = "kwinrc";
+  };
+
+  xdg.configFile."kxkbrc" = {
+    enable = true;
+    source = ./kxkbrc;
+    target = "kxkbrc";
+  };
 }
