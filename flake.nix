db08c74023b0e3fc90419ac7df9e9664b7edacee
Simon Pitt <si_pitt@yahoo.com>
2024-08-31 13:27:48 +0100

    organise dir structure under user and system

--- flake.nix
+++ flake.nix
@@ -12,55 +12,54 @@
       system = "x86_64-linux";
   in {
       nixosConfigurations = {
+
         pistol = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [ 
-            ./hosts/pistol/configuration.nix 
+            ./system/hosts/pistol/configuration.nix 
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.simon = import ./userprofiles/simon-pistol.nix;
+              home-manager.users.simon = import ./user/profiles/simon-pistol.nix;
             }
           ];
         };
+
         rapier = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [ 
-            ./hosts/rapier/configuration.nix
+            ./system/hosts/rapier/configuration.nix
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.simon = import ./userprofiles/simon-rapier.nix;
+              home-manager.users.simon = import ./user/profiles/simon-rapier.nix;
             }
           ];
         };
+
         swing = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [ 
-            ./hosts/swing/configuration.nix
+            ./system/hosts/swing/configuration.nix
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.simon = import ./userprofiles/simon-swing.nix;
+              home-manager.users.simon = import ./user/profiles/simon-swing.nix;
             }
           ];
         };
+
         swingvm = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
-            ./hosts/swingvm/configuration.nix
+            ./system/hosts/swingvm/configuration.nix
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.simon = import ./userprofiles/simon-swing.nix;
+              home-manager.users.simon = import ./user/profiles/simon-swing.nix;
             }
           ];
         };
       };
-
-      # homeConfigurations.simon = home-manager.lib.homeManagerConfiguration {
-      #   pkgs = nixpkgs.legacyPackages.${system};
-      #   modules = [ ./homemanager/home.nix ];
-      # };
     };
 }
--- modules/sddm-chili-theme/chili-overlay.nix
+++
@@ -1,7 +0,0 @@
-(final: prev: {
- sddm-chili-theme = prev.sddm-chili-theme.override {
-   themeConfig = {
-     background = /home/simon/.config/sddm/Backgrounds/salty_mountains.png;
-   };
- };
-})
