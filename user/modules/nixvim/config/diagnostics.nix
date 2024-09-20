36199ccf211e1911384dd8dda7cd80356b3fc8ea
Simon Pitt <si_pitt@yahoo.com>
2024-09-20 20:53:09 +0100

    Customize diagnostic signs

--- user/modules/nixvim/config/diagnostics.nix
+++ user/modules/nixvim/config/diagnostics.nix
@@ -1,3 +1,19 @@
 {
   plugins.trouble.enable = true;
+
+  extraConfigLua = ''
+    -- define diagnostic signs
+    local function sign_define(args)
+      vim.fn.sign_define(args.name, {
+        texthl = args.name,
+        text = args.text,
+        numhl = "",
+      })
+    end
+
+    sign_define({ name = 'DiagnosticSignError', text = '✘' })
+    sign_define({ name = 'DiagnosticSignWarn', text = '▲' })
+    sign_define({ name = 'DiagnosticSignHint', text = '⚑' })
+    sign_define({ name = 'DiagnosticSignInfo', text = '»' })
+  '';
 }
