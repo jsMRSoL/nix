3271c7799251874c83bb4bc3fc9c9f4eb334c3f7
Simon Pitt <si_pitt@yahoo.com>
2024-09-22 20:05:13 +0100

    update swingvm to include nixvim

--- flake.nix
+++ flake.nix
@@ -67,12 +67,14 @@
 
         swingvm = nixpkgs.lib.nixosSystem {
           inherit system;
+          specialArgs = { inherit inputs; };
           modules = [
             ./system/hosts/swingvm/configuration.nix
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.simon = import ./user/profiles/simon-swing.nix;
+              home-manager.extraSpecialArgs = { inherit nixvim; };
             }
           ];
         };
