721673d56ead4537c61b794df79e764b99a54157
Simon Pitt <si_pitt@yahoo.com>
2024-10-03 20:50:45 +0100

    set sddm background

--- system/hosts/viv/configuration.nix
+++ system/hosts/viv/configuration.nix
@@ -1,39 +1,41 @@
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../components/bootloader_systemd_efi.nix
-      ../../components/core.nix
-      ../../components/backlight.nix
-      ../../components/garbage_collection.nix
-      ../../components/sddm.nix
-      ../../components/sound.nix
-      # ../../components/sway.nix
-      ../../components/hyprland.nix
-      ../../components/wayland.nix
-    ];
-
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../components/bootloader_systemd_efi.nix
+    ../../components/core.nix
+    ../../components/backlight.nix
+    ../../components/garbage_collection.nix
+    ../../components/sddm.nix
+    ../../components/sound.nix
+    # ../../components/sway.nix
+    ../../components/hyprland.nix
+    ../../components/wayland.nix
+  ];
 
   hardware.graphics.enable = true;
 
   networking.hostName = "viv"; # Define your hostname.
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = [];
+  environment.systemPackages = [ ];
 
   # overlays
   nixpkgs.overlays = [
     (final: prev: {
-       sddm-chili-theme = prev.sddm-chili-theme.override {
-         themeConfig = {
-           background = "${../../../assets/nix-wallpaper-moonscape.png";
-           ScreenWidth = 1920;
-           ScreenHeight = 1080;
-         };
-       };
+      sddm-chili-theme = prev.sddm-chili-theme.override {
+        themeConfig = {
+          background = "${../../../assets/nix-wallpaper-nineish-dark-gray.png}";
+          ScreenWidth = 1920;
+          ScreenHeight = 1080;
+        };
+      };
     })
   ];
 
