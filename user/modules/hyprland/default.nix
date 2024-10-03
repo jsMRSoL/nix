7d813c5d98318c39041a0f1960099a90ea4991c6
Simon Pitt <si_pitt@yahoo.com>
2024-10-03 22:06:33 +0100

    enable dpms on key and mouse

--- user/modules/hyprland/default.nix
+++ user/modules/hyprland/default.nix
@@ -98,6 +98,8 @@
       misc = {
         disable_splash_rendering = true;
         disable_hyprland_logo = true;
+        mouse_move_enables_dpms = true;
+        key_press_enables_dpms = true;
       };
 
       windowrulev2 = [
