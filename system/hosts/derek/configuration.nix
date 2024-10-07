5d7c996661ed0b03a4759cc74cf90f8588f262e7
Simon Pitt <si_pitt@yahoo.com>
2024-10-07 20:59:19 +0100

    add derek config

---
+++ system/hosts/derek/configuration.nix
@@ -0,0 +1,45 @@
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../components/bootloader_systemd_efi.nix
+    ../../components/core.nix
+    ../../components/backlight.nix
+    ../../components/garbage_collection.nix
+    ../../components/sddm.nix
+    ../../components/sound.nix
+    ../../components/dell-bios-fan-control.nix
+    # ../../components/sway.nix
+    ../../components/hyprland.nix
+    ../../components/wayland.nix
+    # ../../components/postgres.nix
+  ];
+
+  hardware.graphics.enable = true;
+
+  networking.hostName = "derek"; # Define your hostname.
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = [ ];
+
+  # overlays
+  nixpkgs.overlays = [
+    (final: prev: {
+      sddm-chili-theme = prev.sddm-chili-theme.override {
+        themeConfig = {
+          background = "${../../../assets/nix-wallpaper-nineish-dark-gray.png}";
+          ScreenWidth = 1920;
+          ScreenHeight = 1080;
+        };
+      };
+    })
+  ];
+
+  system.stateVersion = "24.05"; # Don't touch! I mean it!
+}
---
+++ user/profiles/simon-derek.nix
@@ -0,0 +1,42 @@
+{ lib, ... }:
+{
+
+  imports = [
+    ../modules/atuin
+    ../modules/bash
+    ../modules/browser
+    ../modules/clipboard
+    ../modules/cmdline
+    ../modules/dunst
+    ../modules/emacs
+    ../modules/foot
+    ../modules/games
+    ../modules/git
+    ../modules/gui
+    ../modules/hyprland
+    ../modules/languages
+    ../modules/neovim
+    ../modules/sway
+    # ../modules/syncthing
+    ../modules/theme
+    ../modules/tmux
+    ../modules/tmux-scripts
+    ../modules/waybar
+    ../modules/yazi
+  ];
+
+  home = {
+    username = "simon";
+    homeDirectory = "/home/simon";
+    stateVersion = "24.05";
+  };
+
+  gui_programs.handbrake.enable = lib.mkForce false;
+  gui_programs.insomnia.enable = lib.mkForce false;
+  gui_programs.mkvtoolnix.enable = lib.mkForce false;
+  gui_programs.simple-scan.enable = lib.mkForce false;
+  gui_programs.sqlitebrowser.enable = lib.mkForce false;
+  gui_programs.zed-editor.enable = lib.mkForce false;
+
+  programs.home-manager.enable = true;
+}
