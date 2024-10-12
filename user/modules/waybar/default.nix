a8d81414d2b47d8f4c4477646cfda62086ea94a0
Simon Pitt <si_pitt@yahoo.com>
2024-10-12 22:35:17 +0100

    simplify waybar keyboard selection logic

--- user/modules/waybar/default.nix
+++ user/modules/waybar/default.nix
@@ -1,24 +1,10 @@
-{ pkgs, osConfig, ... }:
+{ pkgs, hostConfig, ... }:
 let
-  keyboards = {
-    "derek" = "usb-hid-keyboard";
-    "viv" = "at-translated-set-2-keyboard";
-    "swing" = "at-translated-set-2-keyboard";
-    "swingvm" = "at-translated-set-2-keyboard";
-  };
-
-  lookup =
-    attrs: key: default:
-    if attrs ? "${key}" then attrs."${key}" else default;
-
-  currentSystem = osConfig.networking.hostName;
-
-  keyboard = lookup keyboards currentSystem keyboards.viv;
-
   waybar-config = builtins.readFile ./config.jsonc;
 
   customised =
-    builtins.replaceStrings [ "at-translated-set-2-keyboard" ] [ "${keyboard}" ]
+    builtins.replaceStrings [ "at-translated-set-2-keyboard" ]
+      [ "${hostConfig.keyboardType}" ]
       "${waybar-config}";
 in
 {
