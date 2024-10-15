77139585eb3ad7333aa3afbfa2aad6beb990487c
Simon Pitt <si_pitt@yahoo.com>
2024-10-15 22:53:51 +0100

    rapier: customize lightdm

--- system/components/dwm.nix
+++ system/components/dwm.nix
@@ -24,10 +24,24 @@
   ];
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "gb";
-    variant = "";
-    options = "ctrl:nocaps";
+  services.xserver = {
+    xkb = {
+      layout = "gb";
+      variant = "";
+      options = "ctrl:nocaps";
+    };
+    displayManager.lightdm = {
+      background = ../../assets/nix-wallpaper-nineish-dark-gray.png;
+      greeters.gtk = {
+        enable = true;
+        theme = {
+          name = "adw-gtk3-dark";
+          package = pkgs.adw-gtk3;
+        };
+        extraConfig = ''
+          position = 10% 40%
+        '';
+      };
+    };
   };
-
 }
