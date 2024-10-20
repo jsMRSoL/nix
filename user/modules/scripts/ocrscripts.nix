f148d88f772e429ab119e3f77a37b97cec72fa7c
Simon Pitt <si_pitt@yahoo.com>
2024-10-20 22:51:18 +0100

    optimize ocr scripts

--- user/modules/scripts/ocrscripts.nix
+++ user/modules/scripts/ocrscripts.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }:
 let
   ocreng = pkgs.writeShellScriptBin "ocreng" ''
+    for image in *.png; do
+      echo "converting $image..."
+      ${pkgs.imagemagick}/bin/magick "$image" "''${image/.png/.tiff}"
+    done
+
+    for tiff in *.tiff; do
+      echo "unpapering $tiff..."
+      ${pkgs.unpaper}/bin/unpaper "$tiff" "''${tiff/.tiff/.unp}"
+      rm "$tiff"
+    done
+
     count=1
-    for page in *.png; do
+    for page in *.unp; do
+      echo "performing ocr on $page..."
       no=$(printf "%02d" $count)
       ${pkgs.tesseract}/bin/tesseract "$page" "''${page/.png/$no}" -l eng
       count=$((count + 1))
+      rm "$page"
     done
 
     for file in *.txt; do
@@ -15,11 +28,24 @@ let
   '';
 
   ocrlatin = pkgs.writeShellScriptBin "ocrlatin" ''
+    for image in *.png; do
+      echo "converting $image..."
+      ${pkgs.imagemagick}/bin/magick "$image" "''${image/.png/.tiff}"
+    done
+
+    for tiff in *.tiff; do
+      echo "unpapering $tiff..."
+      ${pkgs.unpaper}/bin/unpaper "$tiff" "''${tiff/.tiff/.unp}"
+      rm "$tiff"
+    done
+
     count=1
-    for page in *.png; do
+    for page in *.unp; do
+      echo "performing ocr on $page..."
       no=$(printf "%02d" $count)
-      ${pkgs.tesseract}/bin/tesseract "$page" "''${page/.png/$no}" -l lat+eng
+      ${pkgs.tesseract}/bin/tesseract "$page" "''${page/.unp/_$no}" -l lat+eng
       count=$((count + 1))
+      rm "$page"
     done
 
     for file in *.txt; do
@@ -29,11 +55,24 @@ let
   '';
 
   ocrgreek = pkgs.writeShellScriptBin "ocrgreek" ''
+    for image in *.png; do
+      echo "converting $image..."
+      ${pkgs.imagemagick}/bin/magick "$image" "''${image/.png/.tiff}"
+    done
+
+    for tiff in *.tiff; do
+      echo "unpapering $tiff..."
+      ${pkgs.unpaper}/bin/unpaper "$tiff" "''${tiff/.tiff/.unp}"
+      rm "$tiff"
+    done
+
     count=1
-    for page in *.png; do
+    for page in *.unp; do
+      echo "performing ocr on $page..."
       no=$(printf "%02d" $count)
-      ${pkgs.tesseract}/bin/tesseract "$page" "''${page/.png/$no}" -l grc+eng
+      ${pkgs.tesseract}/bin/tesseract "$page" "''${page/.unp/_$no}" -l grc+eng
       count=$((count + 1))
+      rm "$page"
     done
 
     for file in *.txt; do
