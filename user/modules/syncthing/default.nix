d2eb7d6d03e18dfec40cdd16e8a52a92c3b5c447
Simon Pitt <si_pitt@yahoo.com>
2024-11-03 13:47:21 +0000

    syncthing: replace home.file linking with custom systemd ExecStartPre linking

--- user/modules/syncthing/default.nix
+++ user/modules/syncthing/default.nix
@@ -1,14 +1,20 @@
-{ osConfig, ... }:
+{ pkgs, osConfig, ... }:
 let
   currentSystem = osConfig.networking.hostName;
   configPath = ./. + "/${currentSystem}-config.xml";
+  targetPath = ".local/state/syncthing/config.xml";
 in
 {
   services.syncthing.enable = true;
 
-  home.file."syncthing-config" = {
-    enable = true;
-    source = configPath;
-    target = ".local/state/syncthing/config.xml";
+  systemd.user.services.syncthing.Service = {
+    ExecStartPre = pkgs.writeShellScript "link-syncthing-config.sh" ''
+      if [[ ! -f "$HOME/${targetPath}" ]]; then
+        ln -sT ${configPath} "$HOME/${targetPath}"
+        echo "''$(date): wrote config from nix config" > "$HOME/.local/state/syncthing/nix.log"
+      else
+        echo "''$(date): found existing config.xml." > "$HOME/.local/state/syncthing/nix.log"
+      fi
+    '';
   };
 }
