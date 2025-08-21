34780a14810aa43f9dce3788bfdf309de62d8165
Simon Pitt <si_pitt@yahoo.com>
2025-08-21 21:07:26 +0100

    nix: set git defaults for pull

--- user/modules/git/default.nix
+++ user/modules/git/default.nix
@@ -1,6 +1,10 @@
 {
   programs.git = {
     enable = true;
+    extraConfig = {
+      pull.rebase = true;
+      rebase.autostash = true;
+    };
     userEmail = "si_pitt@yahoo.com";
     userName = "Simon Pitt";
   };
